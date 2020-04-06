3246f8369800d826af998e506d3831fa892b05df
L1zav3ta <61448042+L1zav3ta@users.noreply.github.com>
2020-04-06 16:01:07 +0300

    Add files via upload

---
+++ lameKing.h
@@ -0,0 +1,12 @@
+//прототипы функций
+
+#ifndef lameKing
+#define lameKing
+
+//находит номер строки, в которой введенный элемент
+int searchCurrStr(int countStrs, int countElInStr, int elem);
+
+//выводит путь фигуры
+void stepKing(int *A, int strs, int countElInStr, int S, int F);
+
+#endif // !lameKing
