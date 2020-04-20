31747c6e8b9048b0642f226f8002d1b80124052c
L1zav3ta <61448042+L1zav3ta@users.noreply.github.com>
2020-04-20 16:43:19 +0300

    Update TheDirectedGraph.c
    add the Kruskal algorithm

--- Lab 2 Graphs/TheDirectedGraph.c
+++ Lab 2 Graphs/TheDirectedGraph.c
@@ -3,14 +3,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include "TheDirectedGraph.h"
 
 //Fills the matrix with 0:
-void Matrix_Vertex (int **A, int countVert){
+void Init_Adjencency_Matrix(int **adjencencyMatrix, int countVert) {
 	int i, j;
-	
+
 	for (i = 0; i < countVert; i++) {
 		for (j = 0; j < countVert; j++)
-			A[i][j] = 0;
+			adjencencyMatrix[i][j] = 0;
 	}
 
 	for (i = 0; i < countVert; i++)
@@ -19,38 +20,37 @@ void Matrix_Vertex (int **A, int countVert){
 	for (i = 0; i < countVert; i++) {
 		printf("\n%c", 'A' + i);
 		for (j = 0; j < countVert; j++)
-			printf(" %d  ", A[i][j]);
+			printf(" %d  ", adjencencyMatrix[i][j]);
 	}
 	return;
-} 
+}
 
-//Reads the length of the paths from "a" to "b":
-void Adjencency_Matrix(int **A, int countVert,int startStr, int startCol) {
-	int i, j, len ;	//len
-
+//Reads the weight of the paths from "arrayEdges" to "b":
+void Create_Adjencency_Matrix(int **adjencencyMatrix, int countVert, int startStr, int startCol) {
+	int i, j, weight;	
 	for (i = startStr; i < countVert; i++) {
 		for (j = startCol; j < countVert; j++) {
 			if (i < j) {
-				printf("\n Enter the way length from %c to %c ( between 0 and 99) ", 'A' + i, 'A' + j); 
-				
-				if (!(scanf("%d", &len)) || len < 0 || len > 99) { // !!!ќ“Ћјƒ»“№!!!! чтобы можно было повторить попытку при вводе не числа
-					printf("\nLength error, try again\n");
-					len = 0;
+				printf("\n Enter the way weight from %c to %c ( between 0 and 99) ", 'A' + i, 'A' + j);
+
+				if (!(scanf("%d", &weight)) || weight < 0 || weight > 99) {
+					printf("\nweight error, try again\n");
 					system("pause");
-					Adjencency_Matrix(A, countVert,i,j);
+					return;
+					//Create_Adjencency_Matrix(adjencencyMatrix, countVert, i, j);
 				}
 
-				A[i][j] = len;
-				A[j][i] = len;
+				adjencencyMatrix[i][j] = weight;
+				adjencencyMatrix[j][i] = weight;
 			}
 		}
 	}
 
 	return;
 }
-				
-//Outputs an ajencency matrix with path lengths:
-void Output_Adjencency_Matrix(int **A, int countVert) {
+
+//Outputs an ajencency matrix with path weights:
+void Output_Adjencency_Matrix(int **adjencencyMatrix, int countVert) {
 	int i, j;
 
 	for (i = 0; i < countVert; i++)
@@ -59,19 +59,19 @@ void Output_Adjencency_Matrix(int **A, int countVert) {
 	for (i = 0; i < countVert; i++) {
 		printf("\n%c", 'A' + i);
 		for (j = 0; j < countVert; j++)
-			printf(" %2d", A[i][j]);
+			printf(" %2d", adjencencyMatrix[i][j]);
 	}
 	return;
 }
 
 //Counts the number of non-zero elements 
 //	in the upper-triangular matrix = the number of edges:
-int Count_Edges(int **A, int countVert) {
+int Count_Edges(int **adjencencyMatrix, int countVert) {
 	int i, j, count = 0;
 
 	for (i = 0; i < countVert; i++) {
 		for (j = 0; j < countVert; j++) {
-			if (i < j && A[i][j] != 0)
+			if (i < j && adjencencyMatrix[i][j] != 0)
 				count++;
 		}
 	}
@@ -79,29 +79,72 @@ int Count_Edges(int **A, int countVert) {
 }
 
 //Matrix of edges:
-void Matrix_Edges(int **A1, int countVert, int *A2, int countEdg) {
-	int  p=0, i, j; 
+void Matrix_Edges(int **adjencencyMatrix, int countVert, Edges_t *arrayEdges) {
+	int i, j, p;
 
+	p = 0; 
 	for (i = 0; i < countVert; i++) {
 		for (j = 0; j < countVert; j++) {
-			if (i < j && A1[i][j] != 0) {
-				A2[p] = A1[i][j];
-				printf("%2d", A2[p]);
+			if (i < j && adjencencyMatrix[i][j] != 0) {
+				arrayEdges[p].weight = adjencencyMatrix[i][j];
+				arrayEdges[p].idxStart = i;
+				arrayEdges[p].idxFinish = j;
+				printf("%c -> %c = %d\n", 'A' + arrayEdges[p].idxStart, 'A'+ arrayEdges[p].idxFinish, arrayEdges[p].weight);
 				p++;
 			}
 		}
 	}
 	return;
 }
+				
+//Sort for arrayEdges:
+Edges_t* Sort_Array_Edg(Edges_t *arrayEdges, int countEdg) {
+	int i, j, tmp;
+
+	for (i = 0; i < countEdg; i++) {
+		for (j = i + 1; j < countEdg; j++) {
+			if (arrayEdges[i].weight > arrayEdges[j].weight) {
+				tmp = arrayEdges[i].idxStart;
+				arrayEdges[i].idxStart = arrayEdges[j].idxStart;
+				arrayEdges[j].idxStart = tmp;
+
+				tmp = arrayEdges[i].weight;
+				arrayEdges[i].weight = arrayEdges[j].weight;
+				arrayEdges[j].weight = tmp;
+
+
+				tmp = arrayEdges[i].idxFinish;
+				arrayEdges[i].idxFinish = arrayEdges[j].idxFinish;
+				arrayEdges[j].idxFinish = tmp;
+
+			}
+		}
+	}
 	
+	return arrayEdges;
+}
 
-
-			
-
-			
-			
-/*//Kruskal's algorithm:
-void Kruskal_Algorithm(int **A, int countVert) {
-	int i, j, min;
-
-}*/
+//Kruskal's algorithm:
+void Kruskal_Algorithm(Edges_t *arrayEdges, int countEdges) {
+	int i;
+	
+	arrayEdges = Sort_Array_Edg(arrayEdges, countEdges);
+	
+	printf("Sorted an array of edges:\n");
+	for (i = 0; i < countEdges; i++) 
+		printf("%c -> %c = %d\n", 'A' + arrayEdges[i].idxStart, 'A' + arrayEdges[i].idxFinish, arrayEdges[i].weight);
+	
+	printf("Minimum spanning tree:\n");
+	//output first edge:
+	printf("%c -> %c = %d\n", 'A' + arrayEdges[0].idxStart, 'A' + arrayEdges[0].idxFinish, arrayEdges[0].weight);
+	
+	//output tree without first edge:
+	for (i = 0; i < countEdges; i++) {
+		if (arrayEdges[i].idxFinish != arrayEdges[0].idxStart &&
+				arrayEdges[i].idxFinish != arrayEdges[0].idxFinish &&
+					arrayEdges[i].idxStart != arrayEdges[0].idxFinish) {
+			printf("%c -> %c = %d\n", 'A' + arrayEdges[i].idxStart, 'A' + arrayEdges[i].idxFinish, arrayEdges[i].weight);
+		}
+	}
+	return;
+}
